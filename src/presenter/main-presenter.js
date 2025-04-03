1049d69f7283f9c5d3a96d11d4da83e157b91273
Sergey <gruzdosergey@gmail.com>
2025-04-04 01:49:43 +0500

    module2-task1

--- src/presenter/main-presenter.js
+++ src/presenter/main-presenter.js
@@ -4,13 +4,15 @@ import CreateForm from '../view/create-form-view';
 import Point from '../view/point-view';
 import Sort from '../view/sort-view';
 import Filter from '../view/filter-view';
+import {getRandomNumber} from '../utils';
 
 
 export default class MainPresenter {
-  constructor({ container }) {
+  constructor(container, model) {
     this.container = container;
     this.tripEventsList = document.createElement('ul');
     this.tripEventsList.className = 'trip-events__list';
+    this.pointModel = model;
   }
 
   init(){
@@ -21,12 +23,16 @@ export default class MainPresenter {
 
     this.container.appendChild(this.tripEventsList);
 
-    render(new EditForm(), this.tripEventsList);
+    this.pointModel.points = [...this.pointModel.getPoints()];
+    this.pointModel.offers = [...this.pointModel.getOffers()];
+    this.pointModel.destinations = [...this.pointModel.getDestination()];
+
+    render(new EditForm(this.pointModel,getRandomNumber(this.pointModel.points.length - 1)), this.tripEventsList);
 
     render(new CreateForm(), this.tripEventsList);
 
-    for (let i = 0; i < 3; i++){
-      render(new Point(), this.tripEventsList);
+    for (let i = 1; i < this.pointModel.points.length; i++){
+      render(new Point(this.pointModel, i), this.tripEventsList);
     }
   }
 }
